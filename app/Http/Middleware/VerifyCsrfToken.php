35bcc2e9a4903ca4c1c97f9f49ca34c7a327bb5b
anonymousjuan <zmendozatgdn2021@gmail.com>
2025-03-25 22:32:16 +0800

    csrf

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    protected $except = [
+        'save-data',  // Disable CSRF for this route
+    ];
+}
+
