7915f2259defb27160019f979250fa28845baf76
anonymousjuan <zmendozatgdn2021@gmail.com>
2025-03-25 22:19:56 +0800

    modify route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SensorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/save-data', [SensorController::class, 'store']);
